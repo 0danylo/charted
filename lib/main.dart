290934620c6459d98a2621b027bf916d5ddc1118
Danylo-Voloshyn <danylovoloshyn0@gmail.com>
2024-04-21 19:10:39 -0400

    resetting environment

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:fl_chart/fl_chart.dart';
+import 'package:trend_notes/datum_dialog.dart';
+import 'package:trend_notes/graph_card.dart';
+import 'package:trend_notes/graph_dialog.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
+    return ChangeNotifierProvider<MainPageState>(
+      create: (context) => MainPageState(),
       child: MaterialApp(
-        title: 'Trend Notes',
+        title: 'Chartnote',
         theme: ThemeData(
           useMaterial3: true,
           colorScheme:
@@ -26,11 +28,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyAppState extends ChangeNotifier {
-  var current = 0;
-  List<Map<DateTime, double>> data = List.empty();
-}
-
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
 
@@ -38,24 +35,22 @@ class MainPage extends StatefulWidget {
   State<MainPage> createState() => MainPageState();
 }
 
-class MainPageState extends State<MainPage> {
+class MainPageState extends State<MainPage> with ChangeNotifier {
+  var names = ValueNotifier(List.empty(growable: true));
+  var types = ValueNotifier({});
+  var data = ValueNotifier({});
+
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-
     return Scaffold(
       body: Center(
         child: ListView(children: [
-          for (var datum in appState.data) GraphCard(chartData: datum),
+          for (var name in names.value)
+            GraphCard(name: name, type: types.value[name]!, chartData: data.value[name]!),
           const SizedBox(height: 10),
           Center(
             child: ElevatedButton(
-              onPressed: () => showDialog(
-                  barrierDismissible: true,
-                  context: context,
-                  builder: (BuildContext context) {
-                    return const DataDialog();
-                  }),
+              onPressed: () => makeGraphDialog(context, names, types, data),
               child: const Text('+'),
             ),
           ),
@@ -65,115 +60,16 @@ class MainPageState extends State<MainPage> {
   }
 }
 
-class DataDialog extends StatefulWidget {
-  const DataDialog({super.key});
+makeGraphDialog(context, names, types, data) => showDialog(
+    barrierDismissible: true,
+    context: context,
+    builder: (BuildContext context) {
+      return GraphDialog(names, types, data);
+    });
 
-  @override
-  State<StatefulWidget> createState() => DataDialogState();
-}
-
-class DataDialogState extends State<DataDialog> {
-  var newDate = DateTime.now();
-  var newTime = TimeOfDay.now();
-  var newDatum = 0.0;
-
-  @override
-  Widget build(BuildContext context) {
-    return AlertDialog(
-      content: Column(
-        children: [
-          Title(
-              color: const Color.fromARGB(255, 106, 106, 106),
-              child: const Text("Add data point")),
-          Row(
-            children: [
-              ElevatedButton(
-                  onPressed: () {
-                    var date = showDatePicker(
-                      context: context,
-                      firstDate: DateTime.fromMillisecondsSinceEpoch(0),
-                      lastDate: DateTime.now().add(const Duration(days: 365)),
-                    ).then(
-                        (value) => setState(() => newDate = value ?? newDate));
-                  },
-                  child: const Text("Set date")),
-              Text(newDate.toString()),
-            ],
-          ),
-          Row(
-            children: [
-              ElevatedButton(
-                  onPressed: () {
-                    var time = showTimePicker(
-                            context: context, initialTime: TimeOfDay.now())
-                        .then((value) =>
-                            setState(() => newTime = value ?? newTime));
-                  },
-                  child: const Text("Set time")),
-              Text(newTime.toString()),
-            ],
-          ),
-          TextField(
-            decoration: const InputDecoration(labelText: "Value:"),
-            keyboardType: TextInputType.number,
-            // inputFormatters: [
-            //   FilteringTextInputFormatter.digitsOnly,
-            // ],
-            onChanged: (value) => setState(() => newDatum =
-                RegExp("[+-]?([0-9]*[.])?[0-9]+").firstMatch(value) != null
-                    ? double.parse(value)
-                    : newDatum),
-          ),
-          ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text("Okay"))
-        ],
-      ),
-    );
-  }
-}
-
-class GraphCard extends StatelessWidget {
-  const GraphCard({
-    super.key,
-    required this.chartData,
-  });
-
-  final Map<DateTime, double> chartData;
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final style = theme.textTheme.displayMedium!.copyWith(
-      color: theme.colorScheme.onPrimary,
-    );
-
-    return Card(
-      elevation: 10,
-      color: theme.colorScheme.secondary,
-      child: Column(
-        children: [
-          const Padding(padding: EdgeInsets.all(20.0)),
-          Text(chartData.toString(), style: style),
-          LineChart(
-            LineChartData(),
-          ),
-          Row(
-            children: [
-              ButtonBar(
-                children: [
-                  IconButton(
-                      onPressed: () {}, icon: const Icon(Icons.abc_outlined)),
-                  IconButton(
-                      onPressed: () {}, icon: const Icon(Icons.ac_unit_sharp))
-                ],
-              )
-            ],
-          )
-        ],
-      ),
-    );
-  }
-}
+makeDatumDialog(context, state, graphName) => showDialog(
+    barrierDismissible: true,
+    context: context,
+    builder: (BuildContext context) {
+      return DatumDialog(graphName: graphName);
+    });
